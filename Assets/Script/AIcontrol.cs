3c71b5a54a4eb587b672ca7143a0384a2a2e8ac4
L.R.Carvalho <54295653+Diinees@users.noreply.github.com>
2021-04-23 23:59:18 -0300

    ia
    23/04

---
+++ Assets/Script/AIcontrol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIcontrol : MonoBehaviour
+{
+    public NavMeshAgent agent; 
+    
+    // Pega um objeto na sena
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        
+        // pega o componente do objeto
+    }
+
+    
+}
---
+++ Assets/Script/Agentmanager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agentmanager : MonoBehaviour
+{
+    GameObject[] agentes; 
+    
+    // Criação de array
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentes = GameObject.FindGameObjectsWithTag("Ai");
+        
+        // Busca os agentes
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+            
+            // Se o mouse for apertado 
+        {
+            RaycastHit hit; // Se tiver colisao
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                
+                // Se a linha do raycast tiver fora de colisão executa o foreach
+            {
+                foreach (GameObject a in agentes) 
+                    a.GetComponent<AIcontrol>().agent.SetDestination(hit.point);
+                
+                // nova posicao no mundo
+            }
+
+
+        }
+
+
+
+    }
+}
